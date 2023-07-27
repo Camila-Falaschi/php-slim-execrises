0e3c40314e9dcbccbabf2ada1f80ea2c433f5b53
camila-falaschi <falaschi.camila@yahoo.com.br>
2023-07-27 20:57:11 +0200

    feat: creating a search functionality

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -13,4 +13,25 @@ class SearchController extends Controller
 
         return $this->render($response, 'default.html', ['albums' => $albums]);
     }
+
+    public function search(Request $request, Response $response)
+    {
+        $albums = json_decode(file_get_contents(__DIR__ . '/../../data/albums.json'), true);
+
+        $queryParams = $request->getQueryParams();
+        $query = $queryParams['q'] ?? null;
+        
+        if ($query) {
+            $albums = array_values(array_filter($albums, function($album) use ($query) {
+                return strpos($album['title'], $query) !== false || strpos($album['artist'], $query) !== false;
+            }));
+        }
+
+        return $this->render($response, 'search.html', [
+            'albums' => $albums,
+            'query' => $query
+        ]);
+    }
+
+    
 }
